5ed1c9038e336a29dd2aeb442de97f97d87ed91f
Naufal Azmi <naufalazmi020@gmail.com>
2025-12-31 11:52:01 +0800

    refine: finishing slicing UI usig tailwind

--- tugas_4/src/components/game-setting.js
+++ tugas_4/src/components/game-setting.js
@@ -1,3 +1,5 @@
+import { OPTION_BORDER } from "../constants/styles.js";
+
 export class GameSetting {
   selectedOptionElement;
   selectedOptionId;
@@ -20,11 +22,12 @@ export class GameSetting {
 
   onClickSettingSelector(event, option) {
     if (this.selectedOptionElement) {
-      this.selectedOptionElement.classList.remove('selected-setting');
-      this.selectedOptionElement.classList.add('unselected-setting');
+      this.selectedOptionElement.classList.remove(...OPTION_BORDER.SELECTED.split(' '));
+      this.selectedOptionElement.classList.add(...OPTION_BORDER.UNSELECTED.split(' '));
     }
     this.selectedOptionElement = event.currentTarget;
-    this.selectedOptionElement.classList.add('selected-setting');
+    this.selectedOptionElement.classList.remove(...OPTION_BORDER.UNSELECTED.split(' '));
+    this.selectedOptionElement.classList.add(...OPTION_BORDER.SELECTED.split(' '));
     this._setOptionSelected(option.id);
   }
 
@@ -40,13 +43,13 @@ export class GameSetting {
     this.options.forEach(option => {
       const optionElement = document.createElement('span');
       optionElement.textContent = option.label;
-      optionElement.classList.add('text-preset-5');
-      optionElement.classList.add('unselected-setting');
+      optionElement.classList.add(...OPTION_BORDER.UNSELECTED.split(' '));
       optionElement.addEventListener('click', (event) => this.onClickSettingSelector(event, option));
       this.elementContainer.appendChild(optionElement);
     });
     this.selectedOptionElement = this.elementContainer.children[0];
-    this.selectedOptionElement.classList.add('selected-setting');
+    this.selectedOptionElement.classList.remove(...OPTION_BORDER.UNSELECTED.split(' '));
+    this.selectedOptionElement.classList.add(...OPTION_BORDER.SELECTED.split(' '));
     this._setOptionSelected(this.options[0].id);
   }
 
---
+++ tugas_4/src/constants/styles.js
@@ -0,0 +1,4 @@
+export const OPTION_BORDER = {
+  SELECTED: 'border-blue-400 border rounded-lg text-preset-5 text-blue-400 px-3 py-2 cursor-pointer',
+  UNSELECTED: 'border-neutral-700 border rounded-lg text-preset-5 text-neutral-0 px-3 py-2 cursor-pointer'
+}
